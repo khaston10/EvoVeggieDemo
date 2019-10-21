c7007e7ff9d8047edbdb610f251fd322b5633093
Kevin Haston <kevohaston@gmail.com>
2019-10-20 20:04:07 -0700

    Added Game speed with control panel. Added Sun and planet1.

--- Evo Veggie/Assets/Scripts/GameMain.cs
+++ Evo Veggie/Assets/Scripts/GameMain.cs
@@ -9,11 +9,26 @@ public class GameMain : MonoBehaviour
     public int foodSpawned;
     public int plantEaters;
     public int gamePoints;
+    public Light star1;
+    public Text GameSpeedDisplayText;
+    public Transform planet1;
+    public Transform star1Body;
 
     private int day = 1;
-    private int stepsPerDay = 300;
     private float step = 0;
     private float timer = 0.0f;
+    private int lengthOfDay = 1440;
+    private float timerSpeedCoefficient = .25f; // This value depends on the lengthOfDay. If Day is 360 the coefficient should be 1.
+    // If lengthOfDay is 2 *360, or 720 the coefficient should be 1/2 or .5, and so on. 
+    private int gameSpeed = 1;
+    private float yPos;
+    private float xPos;
+    private float star1YPos;
+    private float star1XPos;
+    private float planetYPos;
+    private float planetZPos;
+    private float planetXPos;
+    private bool gamePaused = false;
 
 
     // Start is called before the first frame update
@@ -31,19 +46,39 @@ public class GameMain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        // Start timer for day, when the timer reaches length_of_day the timer reasets, if game is paused then do not update time.
+        if (gamePaused != true)
+        {
+            timer += Time.deltaTime;
+        }
         Debug.Log(timer);
 
-        if (timer >= 2)
-        {
-            GetComponent<PlantEaters>().MovePlantEaters();
-            if (timer >= 3)
-            {
-                timer = 0;
-            }
-        }
-        
 
+        // Update panels.
+        GameSpeedDisplayText.text = gameSpeed.ToString();
+
+        // Update planet positions.
+        star1YPos = worldSize * Mathf.Sin(timerSpeedCoefficient * timer);
+        star1XPos = worldSize * Mathf.Cos(timerSpeedCoefficient * timer) + worldSize / 2;
+        star1Body.position = new Vector3(xPos, yPos, worldSize / 2);
+
+        planetZPos = worldSize * -3 * Mathf.Cos(timerSpeedCoefficient * timer);
+        planetYPos = worldSize * Mathf.Cos(timerSpeedCoefficient * timer);
+        planetXPos = worldSize * Mathf.Sin(timerSpeedCoefficient * timer) + worldSize / 2;
+        planet1.position = new Vector3(planetXPos, planetYPos, planetZPos);
+
+        // Move the source of light, star1.
+        star1.intensity = worldSize / 5;
+        yPos = worldSize * Mathf.Sin(timerSpeedCoefficient * timer);
+        xPos = worldSize * Mathf.Cos(timerSpeedCoefficient * timer) + worldSize / 2;
+
+        star1.transform.position = new Vector3(xPos, yPos, worldSize / 2);
+
+        if (timer >= lengthOfDay)
+        {
+            timer = 0;
+            day += 1;
+        }
     }
 
     // Save data to Global Control
@@ -53,6 +88,53 @@ public class GameMain : MonoBehaviour
         GlobalControl.Instance.foodSpawned = foodSpawned;
         GlobalControl.Instance.plantEaters = plantEaters;
         GlobalControl.Instance.gamePoints = gamePoints;
+    }
+
+    // This section handles buttons on screen.
+    public void ClickPause()
+    {
+        if (gamePaused == false)
+        {
+            gamePaused = true;
+            gameSpeed = 0;
+        }
+    }
+
+    public void ClickForward()
+    {
+        // Only makes changes if the lengthOfDay = 360, meaning forward mode was occuring. 
+        if (lengthOfDay == 360)
+        {
+            lengthOfDay = 1440;
+            timerSpeedCoefficient = .25f;
+            timer *= 4;
+            gameSpeed = 1;
+        }
+
+        // only makes changes when this button is pushed while game is paused.
+        if (gamePaused == true)
+        {
+            gamePaused = false;
+        }
+    }
+
+    public void ClickFastForward()
+    {
+        // Only make changes if the lengthOFDay = 1440, meaning fast forward was occuring. 
+        if (lengthOfDay == 1440)
+        {
+            lengthOfDay = 360;
+            timerSpeedCoefficient = 1f;
+            timer /= 4;
+            gameSpeed = 4;
+        }
+
+        // only makes changes when this button is pushed while game is paused.
+        if (gamePaused == true)
+        {
+            gamePaused = false;
+        }
+
     }
 
 
